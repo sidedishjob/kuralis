f5f4efa6bf0186378d06fae2a60b28694f5f650a
sidedishjob <private0102iwa@gmail.com>
2025-05-31 22:35:05 +0900

    不要サンプルファイル削除

--- src/lib/queries/getSampleFurniture.ts
+++
@@ -1,7 +0,0 @@
-// lib/queries/getSampleFurniture.ts
-import { sampleFurniture } from "@/data/sampleFurniture";
-
-export async function getSampleFurniture() {
-	// 将来のDB対応にも備えて async にしておく
-	return sampleFurniture;
-}
--- src/lib/queries/getSampleMaintenanceItems.ts
+++
@@ -1,6 +0,0 @@
-import { sampleMaintenanceItems } from "@/data/sampleMaintenanceItems";
-
-export async function getSampleMaintenanceItems() {
-	// 将来的に Supabase 等で置換可能な構成
-	return sampleMaintenanceItems;
-}

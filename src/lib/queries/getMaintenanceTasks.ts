4eb49d1468d60c820db1e573041824df0b50796b
sidedishjob <private0102iwa@gmail.com>
2025-06-01 12:46:08 +0900

    サンプルメンテナンスデータと関連する取得関数を削除

--- src/lib/queries/getMaintenanceTasks.ts
+++
@@ -1,6 +0,0 @@
-import { sampleMaintenanceTasks } from "@/data/sampleMaintenance";
-
-export async function getMaintenanceTasks() {
-	// 将来的には Supabase fetch に差し替え
-	return sampleMaintenanceTasks;
-}

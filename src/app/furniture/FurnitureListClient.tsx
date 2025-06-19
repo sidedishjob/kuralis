402fb8eb887abbfefc4ff311c9c939310ec93ba4
sidedishjob <private0102iwa@gmail.com>
2025-06-19 09:16:34 +0900

    家具リストの表示において、グリッドの間隔の調整

--- src/app/furniture/FurnitureListClient.tsx
+++ src/app/furniture/FurnitureListClient.tsx
@@ -211,7 +211,7 @@ export default function FurnitureListClient({
 						</div>
 					</div>
 
-					<div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
+					<div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-0.5">
 						{filteredFurniture?.map((furniture) => (
 							<FurnitureCard
 								key={furniture.id}

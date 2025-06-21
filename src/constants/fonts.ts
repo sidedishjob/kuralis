9ef1f689b9c6312a9e65b4fe8bea8b530d9be4b5
sidedishjob <private0102iwa@gmail.com>
2025-06-21 22:31:35 +0900

    フォント設定を更新し、InterおよびNoto Sans JPのフォントウェイトを300、400、500、700に変更

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -1,8 +1,12 @@
 import { Inter, Noto_Sans_JP } from "next/font/google";
 
-export const inter = Inter({ subsets: ["latin"], weight: ["700"], variable: "--font-inter" });
+export const inter = Inter({
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "700"],
+	variable: "--font-inter",
+});
 export const notoSansJP = Noto_Sans_JP({
 	subsets: ["latin"],
-	weight: ["700"],
+	weight: ["300", "400", "500", "700"],
 	variable: "--font-noto",
 });

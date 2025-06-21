ab4e243fbc2477880922ed8be9ee51e2da00318f
sidedishjob <private0102iwa@gmail.com>
2025-06-21 22:31:43 +0900

    フッターコンポーネントのテキストを更新し、著作権表示を追加

--- src/components/common/layout/Footer.tsx
+++ src/components/common/layout/Footer.tsx
@@ -4,9 +4,11 @@ import React from "react";
 
 export function Footer() {
 	return (
-		<footer className="py-8 px-6 md:px-12 text-kuralis-500 text-xs font-normal tracking-tighter-custom border-t border-kuralis-200">
+		<footer className="py-8 px-6 md:px-12 text-kuralis-500 text-xs tracking-tighter-custom border-t border-kuralis-200">
 			<div className="container mx-auto">
-				<p>© {new Date().getFullYear()} kuralis</p>
+				<p className="text-xs text-kuralis-500 mt-6">
+					&copy; {new Date().getFullYear()} kuralis. All rights reserved.
+				</p>
 			</div>
 		</footer>
 	);

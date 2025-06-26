691ed91d4a41faf099ec38afb24e642baa5caf10
sidedishjob <private0102iwa@gmail.com>
2025-06-26 09:22:43 +0900

    GETメソッドでのユーザー認証処理をSupabase APIクライアントを使用して実装し、家具メタ情報の取得をユーザーIDに基づいて行うように修正

--- src/app/api/furniture/meta/route.ts
+++ src/app/api/furniture/meta/route.ts
@@ -1,20 +1,32 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getFurnitureMeta } from "@/lib/server/furnitureMeta";
 import { handleApiError } from "@/lib/utils/handleApiError";
+import { createSupabaseApiClient } from "@/lib/supabase/server";
+import { ApiError } from "@/lib/errors/ApiError";
 
 /**
  * 家具メタ情報（カテゴリー・設置場所）の取得
  *
  */
-export async function GET() {
+export async function GET(req: NextRequest) {
+	const res = NextResponse.next();
+
 	try {
-		const { categories, locations } = await getFurnitureMeta();
+		const supabase = await createSupabaseApiClient(req, res);
+		const {
+			data: { user },
+			error,
+		} = await supabase.auth.getUser();
+
+		if (error || !user) throw new ApiError(401, "未認証のため取得できません");
+
+		const { categories, locations } = await getFurnitureMeta(user.id);
 
 		if (categories.length === 0 && locations.length === 0) {
 			throw new Error("家具メタ情報取得エラー:");
 		}
 
-		return NextResponse.json({ categories, locations });
+		return NextResponse.json({ categories, locations }, { status: 200 });
 	} catch (error: unknown) {
 		return handleApiError(error, "カテゴリー・設置場所の取得に失敗しました");
 	}

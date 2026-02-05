99a39a9ad9d4bb8c426b853cbacee4ed0d783240
sidedishjob <private0102iwa@gmail.com>
2026-02-06 07:50:39 +0900

    next/font/google を利用する形に戻し、フォント定義を globals.css の --font-sans に集約

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,21 +9,22 @@ import { Header } from "@/components/common/layout/Header";
 import { Footer } from "@/components/common/layout/Footer";
 import { AuthProvider } from "@/contexts/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
+import { inter, notoSansJP } from "@/constants/fonts";
 import { generateMetadata } from "@/constants/metadata";
 
 export const metadata = generateMetadata();
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="ja">
+		<html lang="ja" className={`${inter.variable} ${notoSansJP.variable}`}>
 			<body className="min-h-screen flex flex-col font-sans">
 				<AuthProvider>
 					<Header />
 					<Suspense>
 						<PageViewListener />
 					</Suspense>
-					<div className="flex-grow flex flex-col">
-						<main className="flex-grow">{children}</main>
+					<div className="grow flex flex-col">
+						<main className="grow">{children}</main>
 						<Toaster />
 					</div>
 					<Footer />

cf3a9cc815a77aef4718c170df3283411fd04cf7
sidedish <private0102iwa@gmail.com>
2026-02-13 22:17:14 +0900

    refactor: authページのレイアウト部分をサーバーコンポーネントに分離

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,18 +1,9 @@
-"use client";
-
-import { PasswordResetForm } from "@/components/auth/PasswordResetForm";
-import { usePasswordResetPage } from "@/hooks/usePasswordResetPage";
+import { ResetPasswordClient } from "./ResetPasswordClient";
 
 export default function ResetPasswordPage() {
-  const { pageState, form, onSubmit } = usePasswordResetPage();
-
   return (
     <div className="flex items-center justify-center p-16">
-      <PasswordResetForm
-        pageState={pageState}
-        form={form}
-        onSubmit={onSubmit}
-      />
+      <ResetPasswordClient />
     </div>
   );
 }

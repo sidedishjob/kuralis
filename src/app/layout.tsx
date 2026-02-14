5cc361d91abe24fef8e9de8a1288f8b331976be9
sidedish <private0102iwa@gmail.com>
2026-02-14 11:45:09 +0900

    change: nextjs quick-win最適化を反映（GA遅延読込と再レンダリング削減）

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,9 +38,9 @@ export default function RootLayout({
           <>
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-              strategy="afterInteractive"
+              strategy="lazyOnload"
             />
-            <Script id="ga-init" strategy="afterInteractive">
+            <Script id="ga-init" strategy="lazyOnload">
               {`
 								window.dataLayer = window.dataLayer || [];
 								function gtag(){dataLayer.push(arguments);}
